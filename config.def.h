58cd1442e6c12672662291c5235cc9372ebf180f
Arkaprabha Chakraborty <arkorty@gmail.com>
2024-03-14 00:55:02 +0530

    Change font face and font size

--- config.def.h
+++ config.def.h
@@ -17,7 +17,7 @@ static bool custom_title = false;
 // scale
 static uint32_t buffer_scale = 1;
 // font
-static char *fontstr = "monospace:size=16";
+static char *fontstr = "JetBrains Mono:size=9";
 // tag names
 static char *tags_names[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
 
